a4912fa497ae3d49c1d2b6da723d2ef08fd4f3c0
justhardinal <just.hardinal@gmail.com>
2015-03-24 17:22:57 +0700

    adding new source code

---
+++ bspringboot/src/main/java/com/hdl/App.java
@@ -0,0 +1,13 @@
+package com.hdl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
